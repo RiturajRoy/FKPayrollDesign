cb7656efaf99ddac7a556ed55c1a784ab71eca2a
Rituraj <f20170957@goa.bits-pilani.ac.in>
2020-05-10 11:50:22 +0530

    Feature04 Adding Employee Creating an abstract or reference class that can be extended by two different sublasses inorder to add the two different types of employees

---
+++ src/AddEmployee.java
@@ -0,0 +1,35 @@
+public abstract class AddEmployee
+{
+    private int empId;
+    private String name;
+    private String address;
+    private String pm;
+    private boolean union;
+    private double salary;
+    private double dues; 
+    private Database db;
+
+    public AddEmployee(int empId, String name, String address,String pm,boolean union, Database db) 
+    {
+        this.empId = empId;
+        this.name = name;
+        this.address = address;
+        this.pm=pm;
+        this.union=union;
+        this.db = db;
+        this.salary=0.0;
+        this.dues=0.0;
+    }
+
+    protected abstract PaymentClassification Classification();
+    protected abstract PaymentSchedule Schedule();
+
+    public void initialize() {
+        PaymentClassification pc = Classification();
+        PaymentSchedule ps = Schedule();
+        Employee e = new Employee(empId, name, address,pm,union);
+        e.pc = pc;
+        e.ps = ps;
+        db.addEmployee(empId, e);
+    }
+}
